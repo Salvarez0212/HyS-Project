90a13377a569b08bbdba1315922ef7cf941ff2b7
Alejandro Arroyave <alvarezsebastian31@gmail.com>
2023-05-31 21:26:20 -0500

    fixing imports

---
+++ app/OurProducts/ourProducts.js
@@ -0,0 +1,24 @@
+const ourProducts = [
+  {
+    id: "1",
+    category: "Para tu zona de cocina",
+    products: ["Cocinas", "Islas", "Barras", "Alacenas", "Torres"],
+  },
+  {
+    id: "2",
+    category: "Para tu habitación",
+    products: ["Closets", "Vestier", "Nocheros"],
+  },
+  {
+    id: "3",
+    category: "Para tu baño",
+    products: ["Mueble superior", "Mueble de lavamanos"],
+  },
+  {
+    id: "4",
+    category: "Para tu zona común",
+    products: ["Centros de entretenimiento", "Escritorios", "Bibliotecas"],
+  },
+];
+
+export default ourProducts;
